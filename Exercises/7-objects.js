2eb29f421d447408265d8b657feee7b84b595859
MytsV <mytsvictoria@gmail.com>
2021-11-26 09:49:48 +0200

    Finished exercise seven

--- Exercises/7-objects.js
+++ Exercises/7-objects.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const a = { name: 'foo' };
+  let b = { name: 'bar' };
+  //can change the inner structure of const value
+  a.name = 'bar';
+  b.name = 'foo';
+  //doesn't work - can't assign a value to a const indentifier
+  //objectFirst = objectSecond;
+  b = a;
+};
 
 module.exports = { fn };
